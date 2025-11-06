c365533ee08925b84f1b0ed78e7fbb3353cfafe3
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2025-11-06 12:22:14 +1100

    for imaging test 11/6/2025 at MU00189065

--- rig dependent/config_timeline_dsRig.m
+++ rig dependent/config_timeline_dsRig.m
@@ -4,9 +4,9 @@ function config_timeline_dsRig
 % 27/9/2019 Created from config_timeline_2prig
 % 10/21/2020 convert to a function, run every time Timeline starts
 
-totalCameraFrameRate = input('What is camera total frame rate in Hz? [70]: ');
+totalCameraFrameRate = input('What is camera total frame rate in Hz? [60]: ');
 if isempty(totalCameraFrameRate)
-    totalCameraFrameRate = 70;
+    totalCameraFrameRate = 60;
 end
 
 % Instantiate the timeline object
@@ -34,19 +34,17 @@ daq_input = @(name, channelID, measurement, terminalConfig) ...
 
 timeline.Inputs = [...
     daq_input('chrono', 'ai8', 'Voltage', 'SingleEnded')... % for reading back self timing wave 
-    daq_input('tlExposeClock', 'ai9', 'Voltage', 'SingleEnded'), ... %from NI to camera to start exposure
-    daq_input('camExposure', 'ai10', 'Voltage', 'SingleEnded'),... %when first row is exposing. originally pcoExposure
-    daq_input('blueLEDmonitor', 'ai11', 'Voltage', 'SingleEnded'),... %turn on blue LED
-    daq_input('purpleLEDmonitor', 'ai12', 'Voltage', 'SingleEnded'),... %turn off purple LED
+    daq_input('tlExposeClock', 'ai1', 'Voltage', 'SingleEnded'), ... %from NI to camera to start exposure
+    daq_input('PCObusy', 'ai10', 'Voltage', 'SingleEnded'),... %
     daq_input('acqLive', 'ai13', 'Voltage', 'SingleEnded'), ... %TL starts acquisition
     daq_input('photoDiode','ai14','Voltage', 'SingleEnded'),... %photodiode measured at screen renamed 30/4/20
-    daq_input('stimScreen', 'ai1', 'Voltage', 'SingleEnded'),... %output to backlight ctrl. added 30/4/20
-    daq_input('FVAL', 'ai2', 'Voltage', 'SingleEnded'),... %ThorCam is not acquiring
-    daq_input('syncSquare','ai0','Voltage', 'SingleEnded'),... %syncSquare from vs. added 4/5/20 
-    daq_input('treadmill','ctr0','Position','SingleEnded'),... %22/7/20 %Position = TTL pulse, EdgeCount = cumsum(Position)
-    daq_input('ao0','ai6','Voltage','SingleEnded'),... %25/9/20
-    daq_input('laser','ai7','Voltage','SingleEnded'),...
-    daq_input('camStrobe','ai5','Voltage','SingleEnded'); %17/12/20
+    daq_input('syncSquare','ai0','Voltage', 'SingleEnded'),... %syncSquare from vs. added 4/5/20
+    daq_input('laserIn','ai6','Voltage','SingleEnded'),... %25/9/20
+    daq_input('DMDIn','ai4','Voltage','SingleEnded'),... %5/3/25
+    daq_input('DMDOut','ai5','Voltage','SingleEnded'),... %17/12/20
+    daq_input('stimScreen','ai2','Voltage','SingleEnded'),...
+    daq_input('amberLEDmonitor','ai11','Voltage','SingleEnded'),... %05/11/25
+    daq_input('redLEDmonitor','ai12','Voltage','SingleEnded'); %06/11/25
     %< there is a delay between syncSquare and actual output on screen. usually syncsquare is earlier, delay size seems to depend on stimulus protocol 
     ];
     %daq_input('eyeCamStrobe', 'ai5', 'Voltage', 'SingleEnded')...
